3d0ab6cf1e84adb3b02abebbb47d007b542d0506
Peach <scdzwyxst@gmail.com>
2022-10-17 19:22:23 +0800

    feat: add theme context & pages example

---
+++ .dumi/theme/context.ts
@@ -0,0 +1,9 @@
+import { createContext } from 'react';
+
+interface IThemeAntVContext {
+  // TODO: add your context type
+}
+
+const ThemeAntVContext = createContext<IThemeAntVContext>({});
+
+export default ThemeAntVContext;
