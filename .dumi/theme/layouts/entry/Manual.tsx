d6cdb0752a733ecd2d471e48c21e8f246890c15d
hustcc <i@hust.cc>
2022-10-18 16:37:31 +0800

    feat: add manual entry and component

---
+++ .dumi/theme/layouts/entry/Manual.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useSidebarData } from 'dumi';
+import { ManualContent } from '../../slots/ManualContent';
+import { Header } from '../../slots/Header';
+import { Footer } from '../../slots/Footer';
+
+export type ManualProps = {
+  readonly children: any;
+}
+
+/**
+ * Manual 路由下的入口
+ */
+export const Manual: React.FC<ManualProps> = ({ children }) => {
+  return (
+    <>
+      <Header isHomePage={false} />
+      <ManualContent> {children} </ManualContent>
+      <Footer />
+    </>
+  );
+}
