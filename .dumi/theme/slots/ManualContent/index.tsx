be2e404b96c330976a24ff0677cddffc2cf3a1f3
BoyYangZai <2365539910@qq.com>
2022-10-21 17:10:52 +0800

    feat：add documentContent

--- .dumi/theme/slots/ManualContent/index.tsx
+++ .dumi/theme/slots/ManualContent/index.tsx
@@ -1,19 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Layout, Affix, BackTop, Menu } from 'antd';
 import { useMedia } from 'react-use';
 import Drawer from 'rc-drawer';
 
 import { useSidebarData } from 'dumi';
 import { useNavigate } from "react-router-dom";
-import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
+import { MenuFoldOutlined, MenuUnfoldOutlined, VerticalAlignTopOutlined } from '@ant-design/icons';
 
 import 'rc-drawer/assets/index.css';
 import styles from './index.module.less';
-
+import { NavigatorBanner } from './NavigatorBanner';
 export type ManualContent = {
   readonly children: any;
 };
 
+interface PreAndNext {
+  slug?: string | undefined,
+  title?: string | undefined
+}
 /**
  * 文档的结构
  */
@@ -26,7 +30,8 @@ export const ManualContent: React.FC<ManualContent> = ({ children }) => {
     return {
       ...item,
       label: item.title,
-      key: item.link
+      key: item.link,
+      title: item.link
     }
   })
   const navigate = useNavigate();
@@ -35,10 +40,59 @@ export const ManualContent: React.FC<ManualContent> = ({ children }) => {
     navigate(e.key)
   };
 
+  const [defaultSelectedKey, setdefaultSelectedKey] = useState([window.location.pathname])
+  const [prev, setPrev] = useState<PreAndNext | undefined>(undefined)
+  const [next, setNext] = useState<PreAndNext | undefined>(undefined)
+  const [currentMenuItem, setCurrentMenuItem] = useState(undefined)
+
+
+  //监听路由去改变selected menu-item
+  useEffect(() => {
+    setdefaultSelectedKey([window.location.pathname])
+  }, [window.location.pathname])
+
+  useEffect(() => {
+    //监听选中的menu-item 拿到 prev and next 
+    getPreAndNext()
+  }, [defaultSelectedKey])
+
+
+  function getPreAndNext() {
+    const menuNodes = document.querySelectorAll('aside .ant-menu-item');
+    const currentMenuNode = document.querySelector(
+      'aside .ant-menu-item-selected',
+    );
+    setCurrentMenuItem(currentMenuNode?.textContent)
+    const currentIndex = Array.from(menuNodes).findIndex(
+      (node) => node === currentMenuNode,
+    );
+
+    const prevNode =
+      currentIndex - 1 >= 0 ? menuNodes[currentIndex - 1] : undefined;
+    const nextNode =
+      currentIndex + 1 < menuNodes.length
+        ? menuNodes[currentIndex + 1]
+        : undefined;
+
+    setPrev((prevNode
+      ? {
+        slug: prevNode.getAttribute('link') || undefined,
+        title: prevNode.textContent || undefined,
+      }
+      : undefined))
+    setNext((nextNode
+      ? {
+        slug: nextNode.getAttribute('link') || undefined,
+        title: nextNode.textContent || undefined,
+      }
+      : undefined))
+  }
+
   const menu = (
     <Menu
       onClick={onClick}
-      defaultSelectedKeys={['/api']}
+      defaultSelectedKeys={defaultSelectedKey}
+      selectedKeys={defaultSelectedKey}
       defaultOpenKeys={['sub1']}
       mode="inline"
       items={renderSidebar}
@@ -47,6 +101,7 @@ export const ManualContent: React.FC<ManualContent> = ({ children }) => {
       forceSubMenuRender
     />
   );
+
   return (
     <>
       <Layout
@@ -73,7 +128,7 @@ export const ManualContent: React.FC<ManualContent> = ({ children }) => {
                 )
               }
               wrapperClassName={styles.menuDrawer}
-              onChange={(open: Boolean) => setDrawOpen(!!open)}
+              onChange={(open?: Boolean) => setDrawOpen(!!open)}
               width={280}
             >
               {menu}
@@ -83,16 +138,21 @@ export const ManualContent: React.FC<ManualContent> = ({ children }) => {
         </Affix>
 
         <Layout.Content className={styles.content}>
-          <h1 className={styles.title}>项目介绍</h1>
-          <div className={styles.readtime}>阅读时间 6 分钟</div>
-          <div className={styles.markdown}>
-            {children}
+          <div className={styles.contentMain}>
+            <h1 className={styles.title}>{currentMenuItem}
+            </h1>
+
+            <div className={styles.readtime}>阅读时间 6 分钟</div>
+            <div className={styles.markdown}>
+              {children}
+            </div>
+            <div>
+              <div className={styles.preandnext}>
+                <NavigatorBanner type="prev" post={prev} />
+                <NavigatorBanner type="next" post={next} />
+              </div>
+            </div>
           </div>
-          <div className={styles.navigator}>
-            <div className={styles.prev}>Prev</div>
-            <div className={styles.next}>Next</div>
-          </div>
-          <BackTop />
         </Layout.Content>
         <Layout.Sider theme="light">TOC</Layout.Sider>
       </Layout>
--- docs/api/t.md
+++ docs/api/t.md
@@ -8,3 +8,5 @@ import React from 'react';
 
 export default () => <h1>Hello dumi!</h1>;
 ```
+
+## 2231
