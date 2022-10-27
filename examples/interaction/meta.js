4e477b352ac765d55ed37ea9994ebd3cc5f88d54
yuzhanglong <loveyzl1123@gmail.com>
2022-10-27 14:33:45 +0800

    feat: init demo meta data for topic

---
+++ examples/interaction/meta.js
@@ -0,0 +1,11 @@
+/** @type {Omit<ExamplesPage.ExampleTopic, 'examples'>} */
+const meta = {
+  id: 'interaction',
+  title: {
+    zh: '交互语法',
+    en: 'Grammar of Interaction',
+  },
+  icon: 'interaction',
+};
+
+module.exports = meta;
