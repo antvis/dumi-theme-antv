f274724987de40fda6c747980f4103e116f36004
洋 <94534613+BoyYangzai@users.noreply.github.com>
2022-10-28 16:27:41 +0800

    feat: add docs ReadingTime (#47)
    Co-authored-by: hustcc <i@hust.cc>

--- .dumi/theme/slots/ManualContent/ReadingTime.tsx
+++ .dumi/theme/slots/ManualContent/ReadingTime.tsx
@@ -1,20 +1,11 @@
 import React from 'react';
 import { Tag } from 'antd';
-import { useLocale } from 'dumi';
+import { useT } from '../hooks';
 
 const ReadingTime: React.FC<any> = ({ readingTime }) => {
-  const locale = useLocale()
-  const { text = '', time = 0 } = readingTime;
   return (
     <Tag>
-      {locale.id === 'zh'
-        ? text.replace(/(\d+)\smin\sread/, (_: string, min: string) => {
-          if (min) {
-            return `阅读时间约 ${min} 分钟`;
-          }
-          return `阅读时间约 ${Math.ceil(time / 60000)} 分钟`;
-        })
-        : text}
+      {`${useT('阅读时间约')} ${Math.ceil(readingTime / 60000)} ${useT('分钟')}`}
     </Tag>
   );
 };
