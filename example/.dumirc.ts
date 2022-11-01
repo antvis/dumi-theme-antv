3e60dfc7fa9c8df21183cb7d49cce9b2fa0fe477
洋 <94534613+BoyYangzai@users.noreply.github.com>
2022-11-01 11:06:06 +0800

    feat: add favcion and site metaInfo (#63)

--- example/.dumirc.ts
+++ example/.dumirc.ts
@@ -3,6 +3,12 @@ import { repository, version } from './package.json';
 
 export default defineConfig({
   locales: [{ id: 'zh', name: '中文' }, { id: 'en', name: 'English' }],
+  title: 'dumi-theme-antv 主题包',                                        // 网站header标题
+  favicons: ['https://mdn.alipayobjects.com/huamei_qa8qxu/afts/img/A*7svFR6wkPMoAAAAAAAAAAAAADmJ7AQ/original'], // 网站 favicon
+  metas: [                                                              // 自定义 meta 标签  
+    { name: 'keywords', content: 'dumi-theme-antv' },
+    { name: 'description', content: '基于 dumi2 的 AntV 官网主题包' },
+  ],
   themeConfig: {
     title: 'dumi-theme-antv', 
     description: '基于 dumi2 的 AntV 官网主题包',
