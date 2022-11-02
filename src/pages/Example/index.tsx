d001c2da66713adaeb1848be990cebe57a7b6b04
怀策策 <loveyzl1123@gmail.com>
2022-11-02 11:51:01 +0800

    feat: beautify sider style (#81)

--- src/pages/Example/index.tsx
+++ src/pages/Example/index.tsx
@@ -7,9 +7,7 @@ import { ExampleSider } from '../../slots/ExampleSider';
 import { CodeRunner } from '../../slots/CodeRunner';
 import { getDemoInfo } from '../../slots/CodeRunner/utils';
 import { ThemeAntVContext } from '../../context';
-
 import styles from './index.module.less';
-
 
 const { Sider, Content } = Layout;
 
@@ -27,14 +25,14 @@ type ExampleParams = {
    * Example 的名称
    */
   example: string;
-}
+};
 
 /**
  * 具体单个案例的页面
  */
 const Example: React.FC = () => {
   const { hash } = useLocation();
-  const nav = useNavigate()
+  const nav = useNavigate();
   const { topic, example } = useParams<ExampleParams>();
   /** 示例页面的元数据信息 */
   const metaData: any = useContext(ThemeAntVContext);
@@ -45,6 +43,7 @@ const Example: React.FC = () => {
 
   const [currentDemo, setCurrentDemo] = useState<ExamplesPage.Demo>();
 
+  const [isCollapsed, setIsCollapsed] = useState<boolean>(false);
 
   useEffect(() => {
     if (topic && example && demo) {
@@ -62,23 +61,43 @@ const Example: React.FC = () => {
           width={250}
           trigger={null}
           collapsible
-          className={styles.sider}
+          collapsed={isCollapsed}
+          className={styles.menuSider}
           theme='light'
         >
-          {currentDemo && <ExampleSider
-            showExampleDemoTitle={true}
-            currentDemo={currentDemo}
-            onDemoClicked={(example) => {
-              const { id: demoId, targetExample, targetTopic } = example;
-              // eg: /zh/examples/case/area/#area1
-              const newURL = `/${locale.id}/examples/${targetTopic?.id}/${targetExample?.id}/#${demoId}`;
-              nav(newURL)
-            }}
-            exampleTopics={exampleTopics}
-          />}
+          {currentDemo && (
+            <ExampleSider
+              showExampleDemoTitle={true}
+              currentDemo={currentDemo}
+              onDemoClicked={(example) => {
+                const { id: demoId, targetExample, targetTopic } = example;
+                // eg: /zh/examples/case/area/#area1
+                const newURL = `/${locale.id}/examples/${targetTopic?.id}/${targetExample?.id}/#${demoId}`;
+                nav(newURL);
+              }}
+              exampleTopics={exampleTopics}
+            />
+          )}
         </Sider>
+        {/*//FIXME: 待 ANTD bug 修复后，可以使用下面的代码*/}
+        {/*<LeftOutlined
+          className={styles.trigger}
+          type={isCollapsed ? 'menu-unfold' : 'menu-fold'}
+          onClick={() => {
+
+            setIsCollapsed(!isCollapsed);
+          }}
+          rotate={isCollapsed ? 180 : 0}
+        />*/}
         <Content className={styles.content}>
-          {topic && example && <CodeRunner exampleTopics={exampleTopics} topic={topic} example={example} demo={demo} />}
+          {topic && example && (
+            <CodeRunner
+              exampleTopics={exampleTopics}
+              topic={topic}
+              example={example}
+              demo={demo}
+            />
+          )}
         </Content>
       </Layout>
     </div>
