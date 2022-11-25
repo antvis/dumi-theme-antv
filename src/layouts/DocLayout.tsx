302cbca6a2e7b3d99a0b90005e013c59517993e9
Peach <scdzwyxst@gmail.com>
2022-11-25 17:35:53 +0800

    refactor: handle hash change for scroll (#196)

--- src/layouts/DocLayout.tsx
+++ src/layouts/DocLayout.tsx
@@ -11,7 +11,7 @@ import '../slots/_.less';
  * DocuLayout 是 dumi2 的内置 layout 入口，在这里使用页面路径进行区分成自己不同的 Layout。
  */
 export default () => {
-  const { themeConfig } = useSiteData();
+  const { themeConfig, loading } = useSiteData();
   const {
     navs
   } = themeConfig;
@@ -25,7 +25,20 @@ export default () => {
   }, []);
 
   const outlet = useOutlet();
-  const { pathname } = useLocation();
+  const { pathname, hash } = useLocation();
+
+  // 监听 hash 变更，跳转到锚点位置
+  // 同时监听页面 loading 状态，因为路由按需加载时需要等待页面渲染完毕才能找到锚点位置
+  useEffect(() => {
+    const id = hash.replace('#', '');
+
+    if (id) {
+      const elm = document.getElementById(decodeURIComponent(id));
+
+      if (elm) document.documentElement.scrollTo(0, elm.offsetTop - 80);
+    }
+  }, [loading, hash]);
+
   const path = pathname
   // 统一去掉中英文前缀
   let p = path.replace('/zh/', '/').replace('/en/', '/');
@@ -33,14 +46,14 @@ export default () => {
   if (p === '/' || p === '/zh' || p === '/en' || p === '/en/') return <Index />;
 
   // 匹配 navs 中的 docs 路由
-  const docsRoutes = 
+  const docsRoutes =
     navs
     .filter(nav => nav.slug && nav.slug.startsWith('docs/'))
     .map(nav => nav.slug && nav.slug.split('/').find(item => item !== 'docs'));
 
   if (docsRoutes.some(route => p.startsWith(`/${route}`) || p.startsWith(`/docs/${route}`))) {
-    return <Manual>{outlet}</Manual>      
+    return <Manual>{outlet}</Manual>
   }
-  
+
   return outlet;
 };
