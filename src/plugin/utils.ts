ea3813e09e70900c811f34325844f66a6caf701e
Anber <shuangyan_m@hotmail.com>
2023-03-20 20:36:35 +0800

    fix: win路径解析错误

--- src/plugin/utils.ts
+++ src/plugin/utils.ts
@@ -1,5 +1,14 @@
 import * as path from 'path';
 import * as fs from 'fs';
+
+export function winPath(path: string) {
+  const isExtendedLengthPath = /^\\\\\?\\/.test(path);
+  if (isExtendedLengthPath) {
+    return path;
+  }
+  return path.replace(/\\/g, '/');
+}
+
 /**
  * 自定义的 resolve 函数，优先找 tsx，如果找不到则找对应的 js 文件。
  * 因为 dumi-theme-antv 编译之后，全部变成 js 文件
@@ -12,11 +21,11 @@ export function myResolve(p: string, alternateExts: string[] = ['.js']): string 
   for (let i = 0; i < exts.length; i++) {
     const ext = exts[i];
     const fullpath = `${pathname}${ext}`;
-    
+
     // 找到一个，则终止
     // 如果文件不存在，则会 throw error
     try {
-      return require.resolve(fullpath);
+      return winPath(require.resolve(fullpath));
     } catch (e) {}
   }
 
