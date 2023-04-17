00fcb7ed147d65b923fa6c69aff0dc321fd5140d
MiniPear <pearminipro@gmail.com>
2023-04-17 11:21:15 +0800

    feat: support spec tab (#211)

--- example/.dumi/global.ts
+++ example/.dumi/global.ts
@@ -1,7 +1,72 @@
-
 /**
  * 增加自己的全局变量，用于 DEMO 中的依赖，以 G2 为例
  */
 if (window) {
-  (window as any).g2 = require('@antv/g2/lib/index.js');
+  (window as any).g2 = extendG2(require('@antv/g2/lib/index.js'));
+}
+
+// 对 G2 的 Chart 对象进行扩展
+// 1. 可以自定义 spec 的展示内容和 key 的排序
+// 2. 在 render 的时候触发 spec 事件抛出 spec，用于展示 spec
+function extendG2(g2) {
+  const { Chart: G2Chart, ...rest } = g2;
+
+  // 这里只是对顶层 key 进行排序，在 G2 中还需要递归排序
+  const sortKeys = (options) => {
+    const newOptions = sortObject(options);
+    return newOptions;
+  };
+
+  const sortObject = (
+    obj,
+    keys = [
+      'type',
+      'autoFit',
+      'theme',
+      'width',
+      'height',
+      'data',
+      'encode',
+      'transform',
+      'scale',
+      'coordinate',
+      'axis',
+      'legend',
+      'label',
+      'children',
+    ].reverse(),
+  ) => {
+    const autoFit = Object.keys(obj).find((d) => d === 'autoFit');
+    const filter = ([key, _]) =>
+      !autoFit ? true : key === 'width' || key === 'height' ? false : true;
+    return Object.fromEntries(
+      Object.entries(obj)
+        .sort(([a], [b]) => keys.indexOf(b) - keys.indexOf(a))
+        .filter(filter),
+    );
+  };
+
+  class Chart extends G2Chart {
+    options() {
+      if (arguments.length !== 0) return super.options(...arguments);
+      const options = super.options();
+      const { type, children = [], key, ...rest } = options;
+      const topLevel =
+        type === 'view' && children.length === 1
+          ? { ...children[0], ...rest }
+          : { type, children, ...rest };
+      return sortKeys(topLevel);
+    }
+    render() {
+      // 触发自定义事件
+      const event = new CustomEvent('spec', {
+        detail: {
+          options: this.options(),
+        },
+      });
+      window.dispatchEvent(event);
+      return super.render();
+    }
+  }
+  return { ...rest, Chart };
 }
--- src/slots/CodeEditor/index.tsx
+++ src/slots/CodeEditor/index.tsx
@@ -1,13 +1,15 @@
-import ReactDOM from 'react-dom';
-import React, { useRef, useState, useCallback, useEffect } from 'react';
 import MonacoEditor, { loader } from '@monaco-editor/react';
-import { useSiteData, useLocale } from 'dumi';
+import { autoType as d3AutoType, dsvFormat } from 'd3-dsv';
+import { useLocale, useSiteData } from 'dumi';
 import { debounce, noop } from 'lodash-es';
+import { format } from 'prettier';
+import parserBabel from 'prettier/parser-babel';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { bind, clear } from 'size-sensor';
-import { replaceInsertCss, execute, compile } from './utils';
-import { Toolbar, EDITOR_TABS } from './Toolbar';
 import { Loading } from '../Loading';
+import { EDITOR_TABS, Toolbar } from './Toolbar';
 import styles from './index.module.less';
+import { compile, execute, replaceInsertCss } from './utils';
 
 loader.config({
   'vs/nls': {
@@ -76,7 +78,7 @@ export type CodeEditorProps = {
     };
     htmlCodeTemplate?: string;
   };
-}
+};
 
 /**
  * 代码编辑器
@@ -94,15 +96,19 @@ export const CodeEditor: React.FC<CodeEditorProps> = ({
   onError = noop,
   onFullscreen = noop,
 }) => {
-  const locale = useLocale()
-  const { extraLib = '' } = useSiteData().themeConfig.playground;
+  const locale = useLocale();
+  const { themeConfig } = useSiteData();
+  const { es5 = true, showSpecTab = false } = themeConfig;
+  const { extraLib = '' } = themeConfig.playground;
   // 编辑器两个 tab，分别是代码和数据
   const [data, setData] = useState(null);
+  const [spec, setSpec] = useState(null);
   const [code, setCode] = useState(source);
   // monaco instance
   const monacoRef = useRef<any>(null);
   // 文件后缀
-  const fileExtension = relativePath.split('.')[relativePath.split('.').length - 1] || 'js';
+  const fileExtension =
+    relativePath.split('.')[relativePath.split('.').length - 1] || 'js';
   // 菜单栏
   const [editorTabs, setEditorTabs] = useState<EDITOR_TABS[]>([]);
   // 当前选中菜单栏
@@ -127,14 +133,14 @@ export const CodeEditor: React.FC<CodeEditorProps> = ({
       onError(null);
     }
   }, []);
-  
+
   useEffect(() => {
     // 用于上报错误信息，使用 script 执行代码
     if (typeof window !== 'undefined') {
       // Cath error of code.
       (window as any).__reportErrorInPlayground = reportError;
       // Catch error of timeout/raf.
-      window.onerror = reportError
+      window.onerror = reportError;
       // Catch error of  promise.
       window.addEventListener('unhandledrejection', reportError);
     }
@@ -144,28 +150,62 @@ export const CodeEditor: React.FC<CodeEditorProps> = ({
         window.onerror = undefined;
         window.removeEventListener('unhandledrejection', reportError);
       }
-    }
+    };
   }, []);
 
-  const executeCode = useCallback(debounce((v: string) => {
-    if (currentEditorTab !== EDITOR_TABS.JAVASCRIPT) return;
-    if (!v) return;
-    
-    // 1. 先编译代码
-    let compiled;
-    try {
-      compiled = compile(replaceInsertCss(v, locale.id), relativePath);
-    } catch (e) {
-      reportError(e);
-      // 执行出错，后面的步骤不用做了！
-      return;
+  const executeCode = useCallback(
+    debounce((v: string) => {
+      if (currentEditorTab !== EDITOR_TABS.JAVASCRIPT) return;
+      if (!v) return;
+
+      // 1. 先编译代码
+      let compiled;
+      try {
+        compiled = compile(replaceInsertCss(v, locale.id), relativePath, es5);
+      } catch (e) {
+        reportError(e);
+        // 执行出错，后面的步骤不用做了！
+        return;
+      }
+
+      // 2. 执行代码，try catch 在内部已经做了
+      execute(
+        compiled,
+        containerId,
+        playground?.container as string,
+        replaceId,
+      );
+
+    }, 300),
+    [exampleId, currentEditorTab],
+  );
+
+  const updateData = (data) => {
+    if (!data) return;
+    const tabs = showSpecTab
+      ? [EDITOR_TABS.JAVASCRIPT, EDITOR_TABS.SPEC, EDITOR_TABS.DATA]
+      : [EDITOR_TABS.JAVASCRIPT, EDITOR_TABS.DATA];
+    setEditorTabs(tabs);
+    setData(data);
+  };
+
+  // 找到 spec 里面的 online data 并且更新它
+  const updateDataFromSpec = (options) => {
+    if (!options) return;
+    const discoverd = [options];
+    const dataList = [];
+    while (discoverd.length) {
+      const node = discoverd.pop();
+      const { data } = node;
+      if (typeof data === 'object' && data.type === 'fetch') {
+        dataList.push(data);
+      }
+      discoverd.push(...(node.children || []));
     }
+    fetchData(dataList.map((d) => d.value)).then(updateData);
+  };
 
-    // 2. 执行代码，try catch 在内部已经做了
-    execute(compiled, containerId, playground?.container as string, replaceId);
-  }, 300), [exampleId, currentEditorTab]);
-
-  // 案例变化的时候，修改期待吗
+  // 案例变化的时候，修改代码
   useEffect(() => {
     setCode(source);
   }, [exampleId]);
@@ -178,9 +218,12 @@ export const CodeEditor: React.FC<CodeEditorProps> = ({
   useEffect(() => {
     const dom = document.getElementById(containerId);
     if (dom) {
-      bind(dom, debounce(() => {
-        dispatchResizeEvent();
-      }, 100));
+      bind(
+        dom,
+        debounce(() => {
+          dispatchResizeEvent();
+        }, 100),
+      );
     }
     return () => {
       dom && clear(dom);
@@ -201,25 +244,69 @@ export const CodeEditor: React.FC<CodeEditorProps> = ({
     };
   }, []);
 
+  // fetch 多份远程数据，如果有多份合并成一份。
+  const fetchData = async (urls) => {
+    const parseCSV = (response) => {
+      return response.text().then((text) => {
+        return dsvFormat(',').parse(text, d3AutoType);
+      });
+    };
+    const parseJSON = (response) => response.json();
+    const dataList = await Promise.all(
+      urls.map((url) =>
+        fetch(url).then((response) => {
+          const format = url.split('.').pop();
+          if (format === 'csv') return parseCSV(response);
+          return parseJSON(response);
+        }),
+      ),
+    );
+    if (dataList.length <= 1) return dataList[0];
+    return Object.fromEntries(urls.map((url, index) => [url, dataList[index]]));
+  };
+
+  // 切换 example 的时候，切换到代码编辑页面
+  // 用于更新当前 example 的 spec 和 data
+  useEffect(() => {
+    setCurrentEditorTab(EDITOR_TABS.JAVASCRIPT);
+  }, [exampleId]);
+
   // hook 用户的数据
   useEffect(() => {
-    const dataFileMatch = source.match(/fetch\('(.*)'\)/);
+    // 需要匹配首位的换行符，以及 ' 和 "
+    const dataFileMatch = source.match(/fetch\(\s*["|'](.*)["|']\s*\)/);
     if (dataFileMatch && dataFileMatch.length > 0) {
-      fetch(dataFileMatch[1])
-        .then((response) => response.json())
-        .then((data) => {
-          setEditorTabs([EDITOR_TABS.JAVASCRIPT, EDITOR_TABS.DATA]);
-          setData(data);
-        });
+      fetchData([dataFileMatch[1].trim()]).then((data) => {
+        updateData(data);
+      });
     } else {
-      setEditorTabs([EDITOR_TABS.JAVASCRIPT]);
+      const tabs = showSpecTab
+        ? [EDITOR_TABS.JAVASCRIPT, EDITOR_TABS.SPEC]
+        : [EDITOR_TABS.JAVASCRIPT];
+      setEditorTabs(tabs);
     }
   }, [exampleId]);
 
+  // 监听更新 spec 的事件，这是一个定义事件，需要在 .dumi/global.ts 里面 dispatch
+  useEffect(() => {
+    const update = (e) => {
+      const { options } = e.detail as any;
+      setSpec(options);
+      updateDataFromSpec(options);
+    };
+    window.addEventListener('spec', update);
+    return () => {
+      window.removeEventListener('spec', update);
+    };
+  });
+
   // 切换 tab
-  const onTabChange = useCallback((tab) => {
-    setCurrentEditorTab(tab);
-  }, [exampleId]);
+  const onTabChange = useCallback(
+    (tab) => {
+      setCurrentEditorTab(tab);
+    },
+    [exampleId],
+  );
 
   // useEffect(() => {
   //   if (monacoRef.current) {
@@ -228,11 +315,65 @@ export const CodeEditor: React.FC<CodeEditorProps> = ({
   //   }
   // }, [currentEditorTab]);
 
-  const onCodeChange = useCallback((value: string, event) => {
-    if (!event.isFlush && currentEditorTab === EDITOR_TABS.JAVASCRIPT) {
-      setCode(value)
+  const onCodeChange = useCallback(
+    (value: string, event) => {
+      if (!event.isFlush && currentEditorTab === EDITOR_TABS.JAVASCRIPT) {
+        setCode(value);
+      }
+    },
+    [currentEditorTab],
+  );
+
+  const parseFunction = (string) => {
+    return string.replace(/"\<func\>(.*)\<\/func\>"/g, (_, code) => code);
+  };
+
+  // 序列化 JavaScript 对象的时候对 function 进行特殊的标注，
+  // 使得解析该字符串的时候能方便的提取 function 对应的字符串。
+  // { add: (x, y) => x + y } => '{ add: <func>(x, y) => x + y</func> }'
+  const withFunction = (_: string, value: any) => {
+    if (typeof value !== 'function') return value;
+    return `<func>${value.toString().replace(/\n/g, '')}</func>`;
+  };
+
+  const languageOf = (tab) => {
+    switch (tab) {
+      case EDITOR_TABS.JAVASCRIPT:
+      case EDITOR_TABS.SPEC:
+        return 'javascript';
+      case EDITOR_TABS.DATA:
+        return 'json';
+      default:
+        return 'javascript';
     }
-  }, [currentEditorTab]);
+  };
+
+  const valueOf = (tab) => {
+    switch (tab) {
+      case EDITOR_TABS.JAVASCRIPT:
+        return code;
+      case EDITOR_TABS.SPEC:
+        return format(
+          parseFunction(`(${JSON.stringify(spec, withFunction)})`),
+          {
+            plugins: [parserBabel],
+          },
+        );
+      case EDITOR_TABS.DATA:
+        return JSON.stringify(data, null, 2);
+      default:
+        return null;
+    }
+  };
+
+  const defaultOf = (tab) => {
+    switch (tab) {
+      case EDITOR_TABS.JAVASCRIPT:
+        return code;
+      default:
+        return null;
+    }
+  };
 
   return (
     <div className={styles.editor}>
@@ -249,36 +390,38 @@ export const CodeEditor: React.FC<CodeEditorProps> = ({
         onEditorTabChange={onTabChange}
         onToggleFullscreen={onFullscreen}
       />
-      <div
-        className={styles.monaco}
-        style={{ height: 'calc(100% - 36px)' }}
-      >
-        <MonacoEditor
-          language={
-            currentEditorTab === EDITOR_TABS.JAVASCRIPT ? 'javascript' : 'json'
-          }
-          value={currentEditorTab === EDITOR_TABS.JAVASCRIPT ? code : JSON.stringify(data, null, 2)}
-          // defaultValue={code}
-          path={relativePath}
-          loading={<Loading />}
-          options={{
-            readOnly: currentEditorTab === EDITOR_TABS.DATA,
-            automaticLayout: true,
-            minimap: {
-              enabled: false,
-            },
-            scrollBeyondLastLine: false,
-            fixedOverflowWidgets: true,
-            lineNumbersMinChars: 4,
-            showFoldingControls: 'always',
-            foldingHighlight: true,
+      {editorTabs.map((tab) => (
+        <div
+          key={tab}
+          className={styles.monaco}
+          style={{
+            height: 'calc(100% - 36px)',
+            display: tab === currentEditorTab ? 'block' : 'none',
           }}
-          onChange={onCodeChange}
-          onMount={(editor: any) => {
-            monacoRef.current = editor;          
-          }}
-        />
-      </div>
+        >
+          <MonacoEditor
+            language={languageOf(tab)}
+            value={valueOf(tab)}
+            defaultValue={defaultOf(tab)}
+            path={tab}
+            loading={<Loading />}
+            options={{
+              readOnly: tab === EDITOR_TABS.DATA || tab === EDITOR_TABS.SPEC,
+              automaticLayout: true,
+              minimap: { enabled: false },
+              scrollBeyondLastLine: false,
+              fixedOverflowWidgets: true,
+              lineNumbersMinChars: 4,
+              showFoldingControls: 'always',
+              foldingHighlight: true,
+            }}
+            onChange={onCodeChange}
+            onMount={(editor: any) => {
+              monacoRef.current = editor;
+            }}
+          />
+        </div>
+      ))}
     </div>
   );
-}
+};
