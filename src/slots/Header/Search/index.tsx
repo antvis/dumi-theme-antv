7ea4d4b75440ab734eb5390abc3bec391f462b12
ai-qing-hai <wb-xcf804241@antgroup.com>
2023-04-24 14:40:46 +0800

    fix(Search): 优化样式和部分代码

--- src/slots/Header/Search/index.tsx
+++ src/slots/Header/Search/index.tsx
@@ -1,6 +1,5 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React from 'react';
 import { Popover } from 'antd';
-import { debounce } from 'lodash';
 import { useIntl, useSiteSearch } from 'dumi';
 import { SearchOutlined } from '@ant-design/icons';
 import { SearchResult } from './SearchResult';
@@ -10,28 +9,19 @@ import styles from './index.module.less';
 
 export const Search = () => {
   const intl = useIntl();
-  const [open, setOpen] = useState<boolean>(false);
-  const { keywords, setKeywords, result, loading } = useSiteSearch();
-
-  const onClose = useCallback(debounce(() => setOpen(false), 200), []);
-
-  useEffect(() => {
-    setOpen(!!result?.length);
-  }, [result])
+  const { keywords, setKeywords, result } = useSiteSearch();
 
   return (
-    <Popover open={open} placement="topLeft" content={<SearchResult results={getSearchResults(result)} />}>
+    <Popover open={!!keywords && !!result?.length} placement="topLeft" content={<SearchResult results={getSearchResults(result)} />}>
       <label className={styles.search} >
         <SearchOutlined className={styles.icon} />
         <input
-          className={styles.input}
           id="search"
           value={keywords}
-          // onBlur={() => onClose()}
-          onFocus={() => setOpen(!!result?.length)}
+          className={styles.input}
           onChange={(e) => setKeywords(e.target.value)}
           placeholder={intl.formatMessage({
-            id: '搜索…'
+            id: '搜索…',
           })}
         />
       </label >
