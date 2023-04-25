7473a38d22e2d6bbad62a3b2fe3b47b1d017c956
ai-qing-hai <wb-xcf804241@antgroup.com>
2023-04-25 15:52:42 +0800

    fix(Search): 修复搜索框聚焦和失焦的事件

--- src/slots/Header/Search/index.tsx
+++ src/slots/Header/Search/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Popover } from 'antd';
 import { useIntl, useSiteSearch } from 'dumi';
 import { SearchOutlined } from '@ant-design/icons';
@@ -9,16 +10,25 @@ import styles from './index.module.less';
 
 export const Search = () => {
   const intl = useIntl();
+  const [open, setOpen] = useState(false);
   const { keywords, setKeywords, result } = useSiteSearch();
 
+  const onClose = useCallback(_.debounce(() => setOpen(false), 200), []);
+
+  useEffect(() => {
+    setOpen(!!keywords && !!result?.length);
+  }, [result, keywords]);
+
   return (
-    <Popover open={!!keywords && !!result?.length} placement="topLeft" content={<SearchResult results={getSearchResults(result)} />}>
+    <Popover open={open} placement="topLeft" content={<SearchResult results={getSearchResults(result)} />}>
       <label className={styles.search} >
         <SearchOutlined className={styles.icon} />
         <input
           id="search"
           value={keywords}
           className={styles.input}
+          onBlur={() => onClose()}
+          onFocus={() => setOpen(!!keywords && !!result?.length)}
           onChange={(e) => setKeywords(e.target.value)}
           placeholder={intl.formatMessage({
             id: '搜索…',
