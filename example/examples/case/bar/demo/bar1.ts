c572e556ed4fcbd54c25a6d71817e77fe7b28bbc
MiniPear <pearminipro@gmail.com>
2023-04-26 17:11:03 +0800

    fix(spec): parse function and fetch data

--- example/examples/case/bar/demo/bar1.ts
+++ example/examples/case/bar/demo/bar1.ts
@@ -1,3 +1,6 @@
+/**
+ * A recreation of this demo: https://observablehq.com/@d3/horizontal-bar-chart
+ */
 import { Chart } from '@antv/g2';
 
 const chart = new Chart({
@@ -6,15 +9,28 @@ const chart = new Chart({
   autoFit: true,
 });
 
+chart.coordinate({ transform: [{ type: 'transpose' }] });
+
 chart
   .interval()
   .data({
     type: 'fetch',
     value:
       'https://gw.alipayobjects.com/os/bmw-prod/fb9db6b7-23a5-4c23-bbef-c54a55fee580.csv',
+    format: 'csv',
   })
+  .transform({ type: 'sortX', reverse: true })
   .encode('x', 'letter')
-  .encode('y', (d) => d.frequency)
-  .axis('y', { labelFormatter: '.0%', title: 'frequency' });
+  .encode('y', 'frequency')
+  .axis('y', { labelFormatter: '.0%' })
+  .label({
+    text: 'frequency',
+    formatter: '.1%',
+    style: {
+      textAnchor: (d) => (+d.frequency > 0.008 ? 'right' : 'start'),
+      fill: (d) => (+d.frequency > 0.008 ? '#fff' : '#000'),
+      dx: (d) => (+d.frequency > 0.008 ? -5 : 5),
+    },
+  });
 
 chart.render();
