5526e7a974faa4820d8690b66fe594679af01db2
Frank William <65594180+ai-qing-hai@users.noreply.github.com>
2023-05-15 09:11:06 +0800

    fix(Search): 通过 window 进行关闭 (#228)
    Co-authored-by: ai-qing-hai <wb-xcf804241@antgroup.com>

--- src/slots/Header/Search/index.tsx
+++ src/slots/Header/Search/index.tsx
@@ -1,6 +1,5 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import { Popover } from 'antd';
-import { debounce } from 'lodash';
 import { useIntl, useSiteSearch } from 'dumi';
 import { SearchOutlined } from '@ant-design/icons';
 import { SearchResult } from './SearchResult';
@@ -11,26 +10,40 @@ import styles from './index.module.less';
 export const Search = () => {
   const intl = useIntl();
   const [open, setOpen] = useState<boolean>(false);
-  const onClose = useCallback(() => setTimeout(() => setOpen(false), 60), []);
   const { keywords, setKeywords, result } = useSiteSearch();
 
   useEffect(() => {
     setOpen(!!result?.length);
   }, [result]);
 
+  useEffect(() => {
+    const close = (e: any) => {
+      if (!e.target?.className?.match(styles.input)) {
+        setOpen(false);
+      }
+    }
+    if (window) {
+      window.addEventListener('click', close);
+    }
+    return () => {
+      window.removeEventListener('click', close);
+    }
+  }, []);
+
+  const searchResults = useMemo(() => getSearchResults(result), [result]);
+
   return (
-    <Popover open={open} placement="topLeft" content={<SearchResult results={getSearchResults(result)} />}>
+    <Popover open={open} placement="topLeft" destroyTooltipOnHide={{ keepParent: false }} content={<SearchResult results={searchResults} />}>
       <label className={styles.search} >
         <SearchOutlined className={styles.icon} />
         <input
           className={styles.input}
           value={keywords}
           autoComplete="off"
-          onBlur={() => onClose()}
           onFocus={() => setOpen(!!result?.length)}
           onChange={(e) => setKeywords(e.target.value)}
           placeholder={intl.formatMessage({
-            id: '搜索…'
+            id: '搜索…',
           })}
         />
       </label >
