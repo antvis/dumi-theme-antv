01a0fd6443067f71c84b8bee6d0d4a1b464f5210
huiyu.zjt <huiyu.zjt@antgroup.com>
2025-11-12 23:38:48 +0800

    fix: 拖动时的全局覆盖层

--- src/pages/AIPlayground/components/SessionLayout/index.tsx
+++ src/pages/AIPlayground/components/SessionLayout/index.tsx
@@ -1,15 +1,17 @@
-import React, { PropsWithChildren } from 'react';
-import styles from './index.module.less';
-import { ConversationsMenu } from '../ConversationsMenu';
-import { AIChatStore } from '../../../../model/AIChat';
-import { useSnapshot } from 'valtio';
 import classnames from 'classnames';
+import React, { PropsWithChildren, useState } from 'react';
+import SplitPane from 'react-split-pane';
+import { useSnapshot } from 'valtio';
+import { AIChatStore } from '../../../../model/AIChat';
+import { ConversationsMenu } from '../ConversationsMenu';
+import styles from './index.module.less';
 
 type SessionLayoutProps = PropsWithChildren;
 
 function SessionLayout(props: SessionLayoutProps) {
   const { children } = props;
   const snap = useSnapshot(AIChatStore);
+  const [isDragging, setIsDragging] = useState(false);
 
   if (!Array.isArray(children)) {
     return null;
@@ -17,16 +19,41 @@ function SessionLayout(props: SessionLayoutProps) {
 
   return (
     <div className={styles.container}>
-      <ConversationsMenu/>
-      <div className={classnames(styles.msgBox, {
-        [styles.msgBoxHalf]: snap.codeBlock,
-        [styles.msgBoxFull]: !snap.codeBlock
-      })}>{children[0]}</div>
-      {snap.codeBlock &&
-        <div className={styles.taskBox}>{children[1]}</div>
-      }
+      <ConversationsMenu />
+      {snap.codeBlock ? (
+        // @ts-ignore
+        <SplitPane
+          split="vertical"
+          defaultSize={'50vw'}
+          onDragStarted={() => setIsDragging(true)}
+          onDragFinished={() => setIsDragging(false)}
+          primary="second"
+          style={{
+            position: 'unset',
+          }}
+        >
+          <div className={classnames(styles.msgBox)}>{children[0]}</div>
+          <div className={styles.taskBox}>{children[1]}</div>
+        </SplitPane>
+      ) : (
+        <div className={classnames(styles.msgBox, styles.msgBoxFull)}>{children[0]}</div>
+      )}
+      {/* 拖动时的全局覆盖层,避免拖动过程中，鼠标移动到了一个 iframe 或者一个插件渲染的 canvas/svg 上，导致 Split-Pane 组件赖以工作的 mousemove 和 mouseup 事件丢失了。 */}
+      {isDragging && (
+        <div
+          style={{
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            zIndex: 9999,
+            cursor: 'col-resize', // 或 'row-resize' 用于水平分割
+          }}
+        />
+      )}
     </div>
   );
 }
 
-export {SessionLayout};
+export { SessionLayout };
