6f0739c6f87ce60eae80233f8b24c9f4a2ac8bb8
Joel Alan <31396322+lxfu1@users.noreply.github.com>
2026-08-10 18:02:41 +0800

    chore: remove feedback (#349)
    Co-authored-by: 福晋 <liufu.lf@antgroup.com>

--- example/.dumirc.ts
+++ example/.dumirc.ts
@@ -37,7 +37,6 @@ export default defineConfig({
     showSpecTab: true, // 是否在 demo 页展示API文档
     themeSwitcher: 'g2',
     es5: false,
-    feedback: true, // 是否开启用户反馈功能
     petercat: {
       show: true,
       // 以下配置仅在 show 为 true 时生效
